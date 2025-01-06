fe58120055ee5d9aa62cd252829a127e7ef3db2f
Mongkhon <sekmongkol123@gmail.com>
2025-01-07 02:01:42 +0700

    first commit

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,22 @@
+function toggleHamburger(el) {
+  el.classList.toggle("change");
+  const slide = document.getElementById("slide");
+  
+  if (slide.style.display === "none" || slide.style.display === "") {
+    slide.style.display = "block";
+  } else {
+    slide.style.display = "none";
+  }
+}
+
+window.addEventListener("resize", function () {
+  const slide = document.getElementById("slide");
+  if (window.innerWidth >= 768) {
+    slide.style.display = "none"; 
+  }
+  if (window.innerWidth <= 1023) {
+    slide.style.display = "none"; 
+  }else {
+    slide.style.display = "flex"; 
+  }
+});
